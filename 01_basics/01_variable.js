4db00cec8b16e40dcacbd06d14317e6f13e451de
himanshu7379 <shuklahimanshu9454@gmail.com>
2023-09-26 00:45:26 +0530

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+console.log("himanshu");
+const accountid = 7379
+let accountemail = "himanshu@gmail.com"
+var accountpassword = "5698"
+accountcity = "pratapgarh"
+
+
+// phone = 9044828671   can't change because constant variable can't be modify or change;
+
+
+accountemail = "shukla@gmail.com"
+accountpassword = "9044"
+accountcity = "lucknow"
+console.table({accountemail, accountid, accountcity, accountpassword});
+
+
+// pre not to use var because of issue in block scope and functional issues
+
+
---
+++ 01_basics/02_data_type.js
@@ -0,0 +1,24 @@
+"use strict";     //treat all js code as newer version
+
+// alert(3+3)  we are using nodejs, not browser
+
+console.log(30+30); console.log("himanshu");
+let name  = "himanshu";
+let age = 23;
+let ismale = true;
+
+// datatype (primitive)
+
+// number = 2 to 53;
+// BigInt for big number
+// String 
+// Boolean
+// null  standalone value
+// undefined
+// Symbol unique
+
+// object 
+console.log(typeof null);   // object
+console.log(typeof "himanshu")   //string
+console.log(typeof age);     //number
+
