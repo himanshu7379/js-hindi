81176d971b6d0b2534ef54f712920ef29cd398da
himanshu7379 <shuklahimanshu9454@gmail.com>
2023-09-28 20:40:06 +0530

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,39 @@ console.log(booleanislogedin);
 let someNumber = 55;
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
+
+
+//   ************************************************   OPERATION   **************************************************************
+
+let value  = 45;
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log(3+8);
+// console.log(3*8);
+// console.log(3+8);
+// console.log(13-8);
+// console.log(13/8);
+// console.log(13%8);
+// console.log(3**3);
+
+let str1 = "hello"
+let str2 = "himanshu"
+let str3 = str1 + str2 
+console.log(str3)
+console.log("1" + 2);
+console.log(1 + "2"); 
+console.log(1 + 2 + "3"); // if string in last then before the string value treat like a numbers
+console.log("1" + 2 + 3); // if string in first then after the string is also treat like a string
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1 = num2 =  num3 = 2 
+console.log(num1);
+let gamecounter = 100;
+++gamecounter
+
+console.log(gamecounter);
