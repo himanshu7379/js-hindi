f84493d09dd1d4334945bae9348c16dddbb74990
himanshu7379 <shuklahimanshu9454@gmail.com>
2023-10-04 00:16:38 +0530

    summary datatype

--- 01_basics/02_data_type.js
+++ 01_basics/02_data_type.js
@@ -1,9 +1,10 @@
-"use strict";     //treat all js code as newer version
+"use strict"; //treat all js code as newer version
 
 // alert(3+3)  we are using nodejs, not browser
 
-console.log(30+30); console.log("himanshu");
-let name  = "himanshu";
+console.log(30 + 30);
+console.log("himanshu");
+let name = "himanshu";
 let age = 23;
 let ismale = true;
 
@@ -11,16 +12,22 @@ let ismale = true;
 
 // number = 2 to 53;
 // BigInt for big number
-// String 
+// String
 // Boolean
 // null  standalone value
 // undefined
 // Symbol unique
 
-// object 
+// object
 
+console.log(typeof null); // object
+console.log(typeof "himanshu"); //string
+console.log(typeof age); //number
 
-console.log(typeof null);   // object
-console.log(typeof "himanshu")   //string
-console.log(typeof age);     //number
+// datatype
 
+// two type of datatype 1)PRIMITIVE DATATYPE     2)NON PRIMITIVE
+
+// PRIMITIVE  :- String, Number, Boolean, null, undefined, Symbol, BigInt.
+// NON PRIMITIVE  (REFERENCE TYPE):- Array, Object, Function
+// NOTE:-  JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them
