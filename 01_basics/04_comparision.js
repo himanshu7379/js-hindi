db52426e82764d1085661ff4a5fdcee604838554
himanshu7379 <shuklahimanshu9454@gmail.com>
2023-10-03 23:01:37 +0530

    comparisison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 5);
+// console.log(5 <= 9);
+// console.log(5 < 9);
+// console.log(5 >= 9);
+// console.log(5 == 9);
+// console.log(5 >= 9);
+
+
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+// console.log(undefined == null);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+
+// ===
+console.log("56" < 64);
