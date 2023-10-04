814a54fbe919129e6a19c17289a2be453f1dc9ff
himanshu7379 <shuklahimanshu9454@gmail.com>
2023-10-04 14:15:13 +0530

    added something

--- 01_basics/02_data_type.js
+++ 01_basics/02_data_type.js
@@ -31,3 +31,4 @@ console.log(typeof age); //number
 // PRIMITIVE  :- String, Number, Boolean, null, undefined, Symbol, BigInt.
 // NON PRIMITIVE  (REFERENCE TYPE):- Array, Object, Function
 // NOTE:-  JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them
+cnsnajbjbva
